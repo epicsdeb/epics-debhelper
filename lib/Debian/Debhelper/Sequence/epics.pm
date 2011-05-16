f3952afe236a25588ae3286a38a69222078fcced
Michael Davidsaver <mdavidsaver@bnl.gov>
2011-05-16 10:38:02 -0400

    split up dh_installepics

--- lib/Debian/Debhelper/Sequence/epics.pm
+++ lib/Debian/Debhelper/Sequence/epics.pm
@@ -4,11 +4,16 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_before("dh_install", "dh_installepics");
+insert_before("dh_install", "dh_epics_installlibs");
+
+insert_after("dh_install", "dh_rtems_install");
 
 insert_after("dh_perl", "dh_rtems");
 
+# wrapper for dh_strip which excludes RTEMS binaries
 insert_before("dh_strip", "dh_strip_rtems");
 remove_command("dh_strip");
+
+insert_before("dh_lintian", "dh_rtems_lintian");
 
 1
