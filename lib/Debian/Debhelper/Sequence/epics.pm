76bc9f3ecf548d68fa5c327760bd3f4bdc615ab5
Michael Davidsaver <mdavidsaver@gmail.com>
2016-03-09 19:25:46 -0500

    Expand dh_rtems_install as dh_epics_install
    dh_rtems_install was a substitute for deban/rtems-*.install.
    dh_epics_install does this, and lib* and *-dev packages.

--- lib/Debian/Debhelper/Sequence/epics.pm
+++ lib/Debian/Debhelper/Sequence/epics.pm
@@ -4,20 +4,26 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
+# dh_epics_postinstall prevents accidental configure/RELEASE install
 insert_after("dh_auto_install", "dh_epics_postinstall");
 
+# relocate host runtime libs to /usr/lib/
 insert_before("dh_install", "dh_epics_installlibs");
 
-insert_before("dh_install", "dh_rtems_install");
+# handle install for packages w/o a debian/*.install
+insert_after("dh_epics_installlibs", "dh_epics_install");
 
+# emit dependencies for rtems packages
 insert_after("dh_perl", "dh_rtems");
 
 # wrapper for dh_strip which excludes RTEMS binaries
 insert_before("dh_strip", "dh_strip_rtems");
 remove_command("dh_strip");
 
+# inject epics-dev dep for *-dev to match libepics* dep in lib*
 insert_after("dh_shlibdeps", "dh_epicsdep");
 
+# auto-create lintian overrides for rtems packages
 insert_before("dh_lintian", "dh_rtems_lintian");
 
 1
